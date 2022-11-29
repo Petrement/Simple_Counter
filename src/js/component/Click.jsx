0cc0479bf7c5edd652d69383e239e48a42aa16f8
Petrement <lspetrement@gmail.com>
2022-11-29 12:08:37 +0000

    changes

--- src/js/component/Click.jsx
+++ src/js/component/Click.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 const Click = () => {
-    function handleSubmit(e) {
-      e.preventDefault();
-      console.log('Back to Zero');
-    }
     return (
-      <form onSubmit={handleSubmit}>
-        <button type="submit">Click</button>
-      </form>
+        <div class="container">
+        <div class="vertical-center">
+        <div className = "buttons">
+            <button type="button"><i class="fas fa-play"></i></button>
+            <button type="submit"><i class="fas fa-pause"></i></button>
+            <button type="reset"><i class="fas fa-stop"></i></button>
+        </div>
+        </div>
+        </div>
     );
   };
 
